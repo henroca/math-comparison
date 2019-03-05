8badee42085b8c0cd5e81911acb3311f6bcc7d51
Jefferson Ramos <jhr.developer@gmail.com>
2019-03-05 12:52:50 -0300

    add simple cov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require "bundler/setup"
 require "math/comparison"
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
